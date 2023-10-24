5002df894f46bc8a233df296540fdce7e6acff86
Asad Shabir <asadbutt667@gmail.com>
2023-10-25 02:20:48 +0500

    absolute imports added (#10)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@src': './src',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/screens/auth/login/login.jsx
@@ -0,0 +1,88 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+
+export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // handle login logic here
+    console.log('handle login');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.signupText}>Don&apos;t have an account?</Text>
+      <TouchableOpacity onPress={() => console.log('navigate to signup')}>
+        <Text style={styles.link}> Sign up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'orange',
+    borderRadius: 5,
+    marginTop: 20,
+    padding: 15,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: 'gray',
+    borderRadius: 15,
+    borderWidth: 1,
+    fontSize: 18,
+    height: 60,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '80%',
+  },
+  link: {
+    color: 'blue',
+    fontSize: 16,
+    marginTop: 12,
+  },
+  signupText: {
+    fontSize: 16,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
